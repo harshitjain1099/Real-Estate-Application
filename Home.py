fbf0ff9b76178c3a4bba7cb50c8e55ab9cfe7111
harshitjain1099 <harshit.vicky443@gmail.com>
2024-09-19 12:57:31 +0530

    Initial commit

---
+++ Home.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title = "Home page",
+)
+st.write("# Welcome to Real Estate Application ")
+
+st.header("Unlock Data-Driven Decision Making in Real Estate")
+
+st.subheader("Discover accurate property price predictions, personalized recommendations, and actionable market insights with RealEstateInsights")
+
+st.image("lovepik-residential-real-estate-picture_501609150.jpg", use_column_width=True)
+
+st.sidebar.success("select a demo above.")
+
+
+
+
+
